5b666eed21879943c4811ab81e9e19205f7f38a6
Daniël Schenk <danielschenk@gmail.com>
2022-12-22 17:29:08 +0100

    Fixes, add slave test

--- lightlib/light.py
+++ lightlib/light.py
@@ -4,21 +4,23 @@ import time
 import logging
 import smokesignal
 
-_logger = logging.getLogger("light")
-
 
 class Light(threading.Thread):
     PERIOD = 1.0
     _next_id = 0
 
-    def __init__(self):
+    def __init__(self, id=None):
         super().__init__()
         self._sync = threading.Event()
-        self._stop = threading.Event()
-        self._id = self._generate_id()
+        self._stop_flag = threading.Event()
+        if not id:
+            id = self._generate_id()
+        self._id = id
 
         self._master = False
         self._is_on = False
+
+        self._logger = logging.getLogger(f"light {self._id}")
 
         smokesignal.on("sync", self._on_sync)
 
@@ -32,32 +34,40 @@ class Light(threading.Thread):
         self._sync.set()
 
     def run(self):
-        while not self._stop.is_set():
+        while not self._stop_flag.is_set():
             if self._master:
                 # signal shouldn't be set by anyone else - check anyway
                 # also, use this for timing the sync
                 if self._sync.wait(self.PERIOD / 2):
+                    self._logger.error("collision detected, degrading to slave")
                     smokesignal.emit("master-collision-detected",
                                      self._id)
                     self._master = False
                 else:
+                    self._logger.debug("emitting sync")
                     smokesignal.emit("sync")
                 self._blink()
             else:
                 arbiting_timeout = random.random() / 100
                 if self._sync.wait(self.PERIOD / 2 + arbiting_timeout):
+                    self._logger.debug("received sync")
                     self._blink()
                 else:
-                    # prevent collision
+                    self._logger.debug("timed out waiting for sync")
                     if not self._sync.is_set():
-                        _logger.info(f"Light {self._id} electing itself")
+                        self._logger.info("electing itself")
                         self._master = True
                         smokesignal.emit("sync")
+                    else:
+                        self._logger.info("was about to elect itself, "
+                                          "but another light did in the meantime "
+                                          "(collision avoidance)")
                     # compensate for extra delay, to stay in phase
                     self._blink(self.PERIOD / 2 - arbiting_timeout)
             self._sync.clear()
 
     def _blink(self, shine_time=None):
+        self._logger.info("blinking")
         if not shine_time:
             shine_time = self.PERIOD / 2
 
@@ -79,4 +89,4 @@ class Light(threading.Thread):
         return f"light-{self._id}"
 
     def stop(self):
-        self._stop.set()
+        self._stop_flag.set()
--- lightlib/test_light.py
+++ lightlib/test_light.py
@@ -1,18 +1,72 @@
+import pytest
 import threading
 import smokesignal
+import logging
 from . import light
 
 
+@pytest.fixture(autouse=True)
+def clear_smokesignal():
+    smokesignal.clear_all()
+
+
+@pytest.fixture(autouse=True)
+def all_logging(caplog):
+    caplog.set_level(logging.DEBUG)
+
+
+class Factory:
+    def __init__(self):
+        self.lights = []
+
+    def get_light(self, id=None):
+        newlight = light.Light(id)
+        self.lights.append(newlight)
+        return newlight
+
+    def stop_all(self):
+        for mylight in self.lights:
+            mylight.stop()
+            mylight.join()
+
+
+@pytest.fixture
+def factory():
+    f = Factory()
+    yield f
+    f.stop_all()
+
+
 class TestLight:
-    def test_master(self):
-        mylight = light.Light()
+    def test_master(self, factory):
+        mylight = factory.get_light("master")
         mylight.start()
         # first cycle includes arbiter delay, allow for it
         self.assert_event_within(mylight, 0.6, True)
         self.assert_event_within(mylight, 0.51, False)
         self.assert_event_within(mylight, 0.51, True)
         self.assert_event_within(mylight, 0.51, False)
-        mylight.stop()
+
+    def test_slave(self, factory):
+        mylight = factory.get_light("slave")
+        mylight.start()
+
+        sync_recvcnt = 0
+
+        def _on_sync():
+            nonlocal sync_recvcnt
+            sync_recvcnt += 1
+        smokesignal.on("sync", _on_sync)
+
+        sync_emitcnt = 3
+        for _ in range(sync_emitcnt):
+            smokesignal.emit("sync")
+            # allow small delay for thread to run
+            self.assert_event_within(mylight, 0.001, True)
+            self.assert_event_within(mylight, 0.51, False)
+
+        assert sync_recvcnt == sync_emitcnt, "more syncs received than expected, " \
+            "means that light might have turned into a master"
 
     def assert_event_within(self, light: light.Light, timeout, new_state=None):
         event = threading.Event()
